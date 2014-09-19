67ae89e1a21b37681a9d26fdfaa346ab37ce7d39
Wildhoney <adam.timberlake@gmail.com>
2014-09-19 09:43:15 +0100

    Initial working example

--- dist/ng-carousel.js
+++ dist/ng-carousel.js
@@ -10,9 +10,24 @@
     var app = $angular.module('ngCarousel', []);
 
     /**
+     * @constant carouselOptions
+     */
+    app.constant('carouselOptions', {
+
+        DIMENSION_WIDTH: 250,
+        DIMENSION_HEIGHT: 300,
+        DIMENSION_SPACING: 10,
+        PERSPECTIVE: 1000,
+        BACKFACE_VISIBILITY: 'hidden',
+        MAINTAIN_WIDTH: true,
+        GROUP_PICTURES_BY: 3
+
+    });
+    
+    /**
      * @directive Carousel
      */
-    app.directive('carousel', function CarouselDirective() {
+    app.directive('carousel', function CarouselDirective(carouselOptions) {
 
         return {
 
@@ -23,17 +38,123 @@
             restrict: 'EA',
 
             /**
+             * @property require
+             * @type {String}
+             */
+            require: 'ngModel',
+
+            /**
+             * @property scope
+             * @type {Object}
+             */
+            scope: {
+                pictures: '=ngModel'
+            },
+
+            /**
              * @method controller
              * @return {void}
              */
             controller: ['$scope', function controller($scope) {
 
                 /**
-                 * @method createTransform
+                 * @property originalWidth
+                 * @type {Number|null}
+                 */
+                $scope.originalWidth = null;
+
+                /**
+                 * @property originalDimension
+                 * @type {Number|null}
+                 */
+                $scope.originalDimension = null;
+
+                /**
+                 * @method applyCarouselStyles
+                 * @param carouselElement {angular.element}
+                 * @return {void}
+                 */
+                $scope.applyCarouselStyles = function applyCarouselStyles(carouselElement) {
+
+                    console.log($scope.originalDimension);
+
+                    carouselElement.css({
+                        width: $scope.originalDimension + 'px',
+                        height: carouselOptions.DIMENSION_HEIGHT + 'px',
+                        position: 'relative',
+                        perspective: carouselOptions.PERSPECTIVE + 'px',
+                        display: 'block'
+                    });
+
+                };
+
+                /**
+                 * @method appendData
+                 * @param figureElement {angular.element}
+                 * @param model {Array|Object|String}
+                 * @return {void}
+                 */
+                $scope.appendData = function appendData(figureElement, model) {
+                    
+                    if ($angular.isArray(model) && model[0].picture) {
+                        $scope.addPictureModels(figureElement, model);
+                        return;
+                    }
+
+                    $scope.addPicture(figureElement, model);
+
+                };
+
+                /**
+                 * Responsible for adding a simple picture source to the figure element.
+                 *
+                 * @method addPicture
+                 * @param figureElement {angular.element}
+                 * @param picture {String}
+                 * @return {void}
+                 */
+                $scope.addPicture = function addPicture(figureElement, picture) {
+
+                    figureElement.css({
+                        backgroundImage: 'url("' + picture + '")',
+                        backgroundRepeat: 'no-repeat'
+                    });
+
+                };
+
+                /**
+                 * Responsible for adding an array of pictures which are represented as models, potentially
+                 * having a textual caption as well.
+                 *
+                 * @method addPictureModels
+                 * @param figureElement {angular.element}
+                 * @param pictureModels {Array}
+                 * @return {void}
+                 */
+                $scope.addPictureModels = function addPictureModels(figureElement, pictureModels) {
+
+                    $angular.forEach(pictureModels, function forEach(pictureModel) {
+
+                        var divElement = $angular.element('<div />');
+
+                        divElement.css({
+                            backgroundImage: 'url("' + pictureModel.picture + '")',
+                            backgroundRepeat: 'no-repeat'
+                        });
+
+                        figureElement.append(divElement);
+
+                    });
+
+                };
+
+                /**
+                 * @method createTransformElement
                  * @param baseElement {angular.element}
+                 * @param translateZ {Number}
                  * @return {angular.element}
                  */
-                $scope.createTransform = function createTransform(baseElement) {
+                $scope.createTransformElement = function createTransformElement(baseElement, translateZ) {
 
                     var carousel = baseElement.find('section');
 
@@ -43,6 +164,14 @@
                         carousel = $angular.element('<section />');
                         baseElement.append(carousel);
 
+                        carousel.css({
+                            width: '100%',
+                            height: '100%',
+                            position: 'absolute',
+                            transformStyle: 'preserve-3d',
+                            transform: 'translateZ(-' + translateZ + 'px) rotateY(0deg)'
+                        });
+
                     }
 
                     return carousel;
@@ -50,23 +179,42 @@
                 };
 
                 /**
-                 * @method addFigure
+                 * @method addFigureElement
                  * @param baseElement {angular.element}
                  * @param degree {Number}
                  * @param translateZ {Number}
-                 * @return {void}
+                 * @return {angular.element}
                  */
-                $scope.addFigure = function addFigure(baseElement, degree, translateZ) {
+                $scope.addFigureElement = function addFigureElement(baseElement, degree, translateZ) {
 
                     // Render the HTML for the carousel if it hasn't been rendered already.
-                    var transformElement = $scope.createTransform(baseElement),
+                    var transformElement = $scope.createTransformElement(baseElement, translateZ),
                         figureElement    = $angular.element('<figure />');
 
                     transformElement.append(figureElement);
 
-                    // Define the degree for the FIGURE according to its position in the array.
-                    figureElement[0].style.transform = 'rotateY(' + degree + 'deg) translateZ(' + translateZ + 'px)';
+                    figureElement.css({
+                        display: 'block',
+                        position: 'absolute',
+                        width: (carouselOptions.DIMENSION_WIDTH - carouselOptions.DIMENSION_SPACING) + 'px',
+//                        width: (carouselOptions.DIMENSION_WIDTH) + 'px',
+//                        width: '100%',
+                        height: carouselOptions.DIMENSION_HEIGHT + 'px',
+                        backfaceVisibility: carouselOptions.BACKFACE_VISIBILITY,
+                        transform: 'rotateY(' + degree + 'deg) translateZ(' + translateZ + 'px)'
+                    });
+                    
+                    return figureElement;
 
+                };
+
+                /**
+                 * @method removeFigureElements
+                 * @param baseElement {angular.element}
+                 * @return {void}
+                 */
+                $scope.removeFigureElements = function removeFigureElements(baseElement) {
+                    baseElement.find('figure').remove();
                 };
 
             }],
@@ -74,22 +222,57 @@
             /**
              * @method link
              * @param scope {Object}
-             * @param element {Object}
+             * @param carouselElement {angular.element}
              * @return {void}
              */
-            link: function link(scope, element) {
+            link: function link(scope, carouselElement) {
 
-                var dimensionCount  = 8,
-                    dimensionDegree = 360 / dimensionCount,
-                    translateZ      = $math.round((210 / 2) / $math.tan($math.PI / dimensionCount));
+                scope.$watch('pictures', function modelUpdated() {
 
-                console.log(dimensionDegree);
+                    var dimensionCount  = scope.pictures.length,
+                        dimensionDegree = 360 / dimensionCount;
 
-                for (var degree = dimensionDegree; degree <= 360; degree += dimensionDegree) {
+                    if (scope.originalWidth === null) {
 
-                    scope.addFigure(element, degree, translateZ);
-                    
-                }
+                        // For maintaining the width we must save the original width of the carousel.
+                        scope.originalWidth = carouselOptions.DIMENSION_WIDTH * dimensionCount;
+
+                    }
+
+                    if (scope.originalDimension === null) {
+
+                        // Also memorise the original dimension size.
+                        scope.originalDimension = carouselOptions.DIMENSION_WIDTH;
+
+                    }
+
+                    scope.removeFigureElements(carouselElement);
+                    scope.applyCarouselStyles(carouselElement);
+
+                    if (carouselOptions.MAINTAIN_WIDTH) {
+
+                        // If we're maintaining the width then we need to calculate the new width for each
+                        // dimension based on the count of the dimensions.
+                        carouselOptions.DIMENSION_WIDTH = scope.originalWidth / dimensionCount;
+
+                        carouselElement.find('section').css({
+                            left: '-' + ((carouselOptions.DIMENSION_WIDTH - scope.originalDimension) / 2) + 'px'
+                        });
+
+                    }
+
+                    var currentDegree = 0,
+                        translateZ    = $math.round((carouselOptions.DIMENSION_WIDTH / 2) / $math.tan($math.PI / dimensionCount));
+
+                    for (var index = 0; index < dimensionCount; index++) {
+
+                        // Add the figure element to the carousel with its unique degree.
+                        var figureElement = scope.addFigureElement(carouselElement, currentDegree += dimensionDegree, translateZ);
+                        scope.appendData(figureElement, scope.pictures[index]);
+
+                    }
+
+                }, true);
 
             }
 
