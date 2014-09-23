e54dd438ff70f2340ea635d9de534f9b6c35132e
Wildhoney <adam.timberlake@gmail.com>
2014-09-23 20:20:28 +0100

    Removed pictures.json API endPoint

--- example/server/default.js
+++ example/server/default.js
@@ -13,21 +13,6 @@
     app.use(express.static(__dirname + '/..'));
     server.listen($process.env.PORT || 3507);
 
-    app.get('/pictures.json', function(request, response) {
-
-        glob('example/images/*.jpg', {}, function (error, files) {
-
-            files = files.map(function map(file) {
-                return 'images/' + path.basename(file);
-            });
-
-            var groupedPictures = chunk(files, 3);
-            response.send(JSON.stringify(groupedPictures));
-
-        });
-
-    });
-
     app.get('/imgur.json', function(request, response) {
 
         var options = {
